254176e63060c197499d672d6a28c11fb808be13
kishan <kishanreddy226@gmail.com>
2025-10-25 11:40:34 +0530

    first commit

---
+++ src/main/java/com/demo/controller/WelcomeController.java
@@ -0,0 +1,13 @@
+package com.demo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WelcomeController {
+    @GetMapping("/message")
+    public String getMessage()
+    {
+        return "Welcome to my world..!";
+    }
+}
